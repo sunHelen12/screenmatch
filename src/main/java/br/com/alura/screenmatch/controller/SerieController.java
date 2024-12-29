3bd54595d952dae5b280cc61ee95db895bbcd820
sunHelen12 <sunrise.developer12@gmail.com>
2024-12-28 21:00:58 -0300

    Mudanças Atualização de funcionalidades

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +33,9 @@ public class SerieController {
         return servico.obterLancamentos();
     }
 
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id){
+        return servico.obterPorId(id);
+    }
     
 }
--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +27,17 @@ public class SerieService {
     }
 
     public List<SerieDTO> obterLancamentos() {
-        return converteDados(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+        return converteDados(repositorio.encontrarEpisodiosMaisRecentes());
+    }
+
+    public SerieDTO obterPorId(Long id){
+        Optional<Serie> serie = repositorio.findById(id);
+        if(serie.isPresent()){
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), 
+            s.getAvaliacao(), s.getGenero() , s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        return null;
     }
 
     private List<SerieDTO> converteDados(List<Serie> series){
